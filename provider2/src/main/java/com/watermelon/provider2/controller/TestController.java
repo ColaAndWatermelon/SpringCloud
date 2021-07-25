0b4daa7265f27dd9ce73535eff819534bf686070
watermelon <905268497@qq.com>
2021-07-25 22:25:59 +0800

    代码提交-sentinel

--- provider2/src/main/java/com/watermelon/provider2/controller/TestController.java
+++ provider2/src/main/java/com/watermelon/provider2/controller/TestController.java
@@ -4,6 +4,7 @@ import com.watermelon.provider2.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/7/20 20:46
  */
 @RestController
+@RequestMapping("test1")
 public class TestController {
 
     @Autowired
